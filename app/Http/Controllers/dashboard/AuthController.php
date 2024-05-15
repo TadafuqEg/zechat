b44f7d601fb73e445c97ae87d70f5a3301f13364
fathysamir <01154857080fa@gmail.com>
2024-05-15 18:08:42 +0300

    mmm

---
+++ app/Http/Controllers/dashboard/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+use Spatie\Permission\Models\Role;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function login_view()
+    {
+        return view('dashboard.auth.login');
+    }
+
+    public function login(Request $request)
+    {   
+        $validator  =   Validator::make($request->all(), [
+               
+                'email' => ['required', 'string', 'email'],
+                'password' => ['required', 'string'],
+               
+        ]);
+            // dd($request->all());
+        if ($validator->fails()) {
+           
+            return Redirect::back()->withErrors($validator)->withInput($request->all());
+        }
+        //dd($request->all());
+        if (Auth::attempt(['email' => request('email'),'password' => request('password')])){
+
+            return redirect('/home');
+        }else{
+
+            return back()->withErrors(['msg' => 'There is something wrong']);
+        }
+       
+    }
+
+
+///////////////////////////////////////////  Logout  ///////////////////////////////////////////
+
+    public function logout(){
+        Auth::logout();
+       
+       // auth()->guard('admin')->logout();
+        return redirect('/login');
+    }
+
+    public function home(){
+        return view('dashboard.home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Carbon\Carbon;
+use App\Http\Controllers\dashboard\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,60 +16,72 @@ use Carbon\Carbon;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('testing',function(){
-
     
-    
-
-    // Define your two datetime values
-    $datetime1 = Carbon::parse('2023-09-06 10:00:00');
-    $datetime2 = Carbon::parse('2023-09-06 15:30:00');
-
-    // Calculate the difference in hours and minutes
-    $diff = $datetime1->diff($datetime2);
-
-    // Extract hours and minutes from the difference
-    $hours = $diff->h;
-    $minutes = $diff->i;
-
-    // Output the result
-    echo "Total Hours and Minutes Difference: $hours hours and $minutes minutes <br><br><br><br><br>";
-
-
-
-    //-------------------------------------
-
-    // Sample durations in "h:i" format
-    $durations = ["2:30", "3:15", "1:45"];
-
-    // Function to convert "h:i" format to minutes
-    function convertToMinutes($time)
-    {
-        list($hours, $minutes) = explode(':', $time);
-        return $hours * 60 + $minutes;
+    if(!auth()->user()){
+        return redirect('/login');
+    }else{
+        return redirect('/home');
     }
-
-    // Convert "h:i" durations to minutes and sum them up
-    $totalMinutes = array_reduce($durations, function ($carry, $duration) {
-        return $carry + convertToMinutes($duration);
-    }, 0);
-
-    // Calculate total hours and minutes
-    $totalHours = floor($totalMinutes / 60);
-    $totalMinutes %= 60;
-
-    // Format the result as "h:i"
-    $totalTime = Carbon::createFromTime($totalHours, $totalMinutes)->format('H:i');
-
-    // Output the result
-    echo "Total Hours: $totalTime";
-
-
 });
+Route::get('/login', [AuthController::class, 'login_view'])->name('login.view');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::group(['middleware' => ['admin']], function () {
+    Route::get('/home', [AuthController::class, 'home'])->name('home');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    /////////////////////////////////////////
+});
+
+
+// Route::get('testing',function(){
+
+    
+    
+
+//     // Define your two datetime values
+//     $datetime1 = Carbon::parse('2023-09-06 10:00:00');
+//     $datetime2 = Carbon::parse('2023-09-06 15:30:00');
+
+//     // Calculate the difference in hours and minutes
+//     $diff = $datetime1->diff($datetime2);
+
+//     // Extract hours and minutes from the difference
+//     $hours = $diff->h;
+//     $minutes = $diff->i;
+
+//     // Output the result
+//     echo "Total Hours and Minutes Difference: $hours hours and $minutes minutes <br><br><br><br><br>";
+
+
+
+//     //-------------------------------------
+
+//     // Sample durations in "h:i" format
+//     $durations = ["2:30", "3:15", "1:45"];
+
+//     // Function to convert "h:i" format to minutes
+//     function convertToMinutes($time)
+//     {
+//         list($hours, $minutes) = explode(':', $time);
+//         return $hours * 60 + $minutes;
+//     }
+
+//     // Convert "h:i" durations to minutes and sum them up
+//     $totalMinutes = array_reduce($durations, function ($carry, $duration) {
+//         return $carry + convertToMinutes($duration);
+//     }, 0);
+
+//     // Calculate total hours and minutes
+//     $totalHours = floor($totalMinutes / 60);
+//     $totalMinutes %= 60;
+
+//     // Format the result as "h:i"
+//     $totalTime = Carbon::createFromTime($totalHours, $totalMinutes)->format('H:i');
+
+//     // Output the result
+//     echo "Total Hours: $totalTime";
+
+
+// });
 
 
 // Route::get('test-firebase',function(){
@@ -76,10 +89,10 @@ Route::get('testing',function(){
 //     $fireBase->sendFirebaseNotification(notificationBody:['type'=>'you have a new friend request'],token:'d0oA0Tg2TvCLqj_FfvZrU0:APA91bHcLEKf61OscV8-CW8QEQ36PE8iAaGDTSH-usw2R9yYn8Pqw733S4rSVvkU7RQqHayBIwDzITflU1kWd55ff_qhzrKtj0MhrfTb3-QzyRTtMvsLGMMfC-V1Fnd-qAcpLxafguBm');
 // });
 
-Route::get('testing',function(){
-    $user = User::find(208);
-    $userChats = User::whereHas('messages', function ($query) use ($user) {
-        $query->where('sender_id', $user->id)->orWhere('receiver_id', $user->id);
-    })->get();
-    dd($userChats);
-});
+// Route::get('testing',function(){
+//     $user = User::find(208);
+//     $userChats = User::whereHas('messages', function ($query) use ($user) {
+//         $query->where('sender_id', $user->id)->orWhere('receiver_id', $user->id);
+//     })->get();
+//     dd($userChats);
+// });
