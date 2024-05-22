647932a2b46ff90f5d419532bdacba4ba66ddf70
fathysamir <01154857080fa@gmail.com>
2024-05-22 13:56:27 +0300

    mmm

--- resources/views/dashboard/roles/index.blade.php
+++ resources/views/dashboard/roles/index.blade.php
@@ -64,11 +64,11 @@
                             </div>
                         @else
                             
-                               
+                                <div class="row vh-100 bg-secondary rounded align-items-center justify-content-center mx-0">
                                     <div class="col-md-6 text-center">
                                         <h3>This is blank page</h3>
                                     </div>
-                                
+                                </div>
                             
                         @endif
                     </div>
--- resources/views/dashboard/users/index.blade.php
+++ resources/views/dashboard/users/index.blade.php
@@ -62,11 +62,11 @@
                             </div>
                         @else
                             
-                                
+                                <div class="row vh-100 bg-secondary rounded align-items-center justify-content-center mx-0">
                                     <div class="col-md-6 text-center">
                                         <h3>This is blank page</h3>
                                     </div>
-                                
+                                </div>
                             
                         @endif
                     </div>
